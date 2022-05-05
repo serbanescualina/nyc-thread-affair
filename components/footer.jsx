1f23b9875ffd31f7fd0188b0f00657603b795fb7
Serbanescu Alina <stanalinaionela@yahoo.ro>
2022-05-05 22:27:01 +0300

    curs 05.05.2022

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,3 +1,151 @@
 export default function Footer(){
-  return <footer>f </footer>
+  return <footer className="footer">
+    <section className="footer-primary">
+      <div className="footer-primary-inner container">
+        <section className="footer-nav">
+          <ul>
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+          </ul>
+
+          <ul>
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+          </ul>
+
+          <ul>
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+          </ul>
+
+          <ul>
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+
+            <li>
+              <a href="" title="Link">Link text</a>
+            </li>
+          </ul>
+          </section>
+
+          <section className="footer-support">
+            <ul>
+              <li>
+                <a href="" title="Support">
+                  <i className="fa-solid fa-phone"></i>
+                  Support</a>
+              </li>
+
+              <li>
+                <a href="" title="Support">
+                  <i className="fa-solid fa-phone"></i>
+                  Support</a>
+              </li>
+
+              <li>
+                <a href="" title="Support">
+                  <i className="fa-solid fa-phone"></i>
+                  Support</a>
+              </li>
+            </ul>
+          </section>
+          <section className="footer-contact">
+            <p>
+              Largo Augusto 3, Milano, 20122, Milano, IT Tel: 0039 02 760 003 66
+              office@thread-affair.com
+            </p>
+          </section>
+
+      </div>
+    </section>
+
+    <section className="footer-secondary text-center">
+      <p className="mb-2">©2019 THREAD AFFAIR. All Rights Reserved.</p>
+
+      <p className="d-flex align-items-center justify-content-center"
+      >Designed by
+           <img src="/images/pixellab_logo.jpg" alt="Pixellab"  className="ms-1"></img>
+        </p>
+    </section>
+     </footer>
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,8 @@
 export default function Header(){
   return (
-    <header className="header container mt-4">
-      <section className="header-secondary d-flex justify-content-between">
-        <div className="header-connect">
+    <header className="header container mt-0 mt-lg-4">
+      <section className="header-secondary">
+        <div className="header-connect d-none d-lg-block mt-lg">
           <ul>
             <li>
               <a href="tel:+1-877-666-1840" title="Call us"><i className="fa-solid fa-phone"></i>Toll Free 1-877-666-1840</a>
@@ -13,11 +13,25 @@ export default function Header(){
             </li>
           </ul>
         </div>
+        <div className="header-search d-lg-none">
+          <ul>
+            <li>
+              <a href="" title="Menu">
+              <i className="fa-solid fa-bars"></i>
+              </a>
+            </li>
 
+            <li>
+              <a href="" title="Search">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
         <div className="header-controls">
           <section className="section user-controls">
             <a href="" title="Account"></a>
-            <img src="/images/user-avatar.png" alt="User" />
+            <img src="/images/user-avatar.png" alt="User"></img>
             <i className="fa-solid fa-angle-down"></i>
           </section>
           <section className="cart-controls">
@@ -25,9 +39,57 @@ export default function Header(){
             <span className="cart-qty">3</span></a>
           </section>
         </div>
+
+        <div className="header-logo text-center">
+          <a href="https://nbc.com" title="logo">
+            <img src="/images/logo.jpg" alt="Thresd Affair"></img>
+          </a>
+        </div>
+
       </section>
 
-      <section className="header-primary">  </section>
+      <section className="header-primary d-none d-lg-block">
+
+      <nav className="nav-primary">
+        <ul>
+          <li>
+            <a href="" title="Costume">Costume</a>
+          </li>
+
+          <li>
+            <a href="" title="Costume">Costume</a>
+          </li>
+
+          <li>
+            <a href="" title="Costume">Costume</a>
+          </li>
+
+           <li>
+            <a href="" title="Costume">Costume</a>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className="nav-secondary">
+        <ul>
+          <li>
+            <a href="" title="Office suits">Office suits</a>
+          </li>
+
+          <li>
+            <a href="" title="Office suits">Office suits</a>
+          </li>
+
+          <li>
+            <a href="" title="Office suits">Office suits</a>
+          </li>
+
+          <li>
+            <a href="" title="Office suits">Office suits</a>
+          </li>
+        </ul>
+      </nav>
+       </section>
      </header>
   )
 }
